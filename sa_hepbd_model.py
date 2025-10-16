17ef7893399d3a5bc862a38e35b37cc336a90516
Chris S <chris.seaman@burnet.edu.au>
2025-10-16 12:24:26 +1100

    code clean

--- sa_hepbd_model.py
+++ sa_hepbd_model.py
@@ -6,7 +6,7 @@ import hbv.constants as const
 
 
 # Set Own Save Directory
-#save_path  = 
+#save_path  =
 
 # Creates directory (if not already existing)
 save_dir = pathlib.Path(save_path)
@@ -20,54 +20,20 @@ hbv.utils.hepbd_scenarios_ZAF(const.db_path, const.calib_path, const.calibration
 # Data Analysis (Data for Plots and Tables Returned)
 
 hbv.utils.zaf_epi_outcomes(res_save_dir) # Epidemiological Outcomes
-hbv.zaf_economic_analysis(res_save_dir, h_disc = 0.03, c_disc = 0.03) # Economic outcomes, returns .pkl for cost-effectiveness fronteirs 
+hbv.zaf_economic_analysis(res_save_dir, h_disc=0.03, c_disc=0.03) # Economic outcomes, returns .pkl for cost-effectiveness fronteirs
 
-# Generate Outcome Plots
+# Generate Outcome Plots (main analysis)
 hbv.utils_plotting.cost_eff_frontier(res_save_dir) # Figure 1, Figure 2
 
+# One Way Sensitivity Analysis (main analysis Figure 3)
+owsa_runs = hbv.sensitivity.owsa_mtct_run(db_path, calib_path, calibration) #run OWSA scenarios
+hbv.sensitivity.owsa_mtct_outcomes(owsa_runs, res_save_dir) # calculate and plot outcomes
+
+# Supplemental Figures
+hbv.utils_plotting.valid_plots(res_save_dir, calib_path, calibration, db_path)  # HBV deaths validation plot
+hbv.utils_plotting.transmission_plots(res_save_dir)  # Transmission modality in baseline scenario
 
 
 
 
 
-
-country = "ZAF" 
-
-
-
-
-
-db_path = _get_gitlab_folder()+"databooks/"
-res_name = "20250918_add_negvax"
-calib_path = _get_gitlab_folder()+"calibrations/revisions/"
-calibration = "ZAF_calibration_vcont_test.xlsx" # denotes calibration to be used for this analysis
-
-res_save_dir = pathlib.Path(_get_sharepoint_folder()+f"Applications/South Africa/results/{res_name}") #folder where results get saved
-res_save_dir.mkdir(parents=True, exist_ok = True)
-
-# Generate Databook
-#hbv.generate_databook.generate_databook(country, savedir=db_path)
-
-# Run central analyses only
-## res_store = hbv.utils.central_run_only(db_path, calib_path, calibration)
-#Save central run results
-## test_save_dir = pathlib.Path(_get_sharepoint_folder()+f"Applications/South Africa/results/tests/")
-## test_save_dir.mkdir(parents=True, exist_ok=True)
-## save_name = "MTCT_test"
-## hbv.utils_plotting.testing_central_plots(res_store, test_save_dir, save_name)
-
-# Run scenarios and simulations
-hbv.utils.hepbd_scenarios_ZAF(db_path, calib_path, calibration, res_save_dir)
-
-
-
-#Calibration Plots
-hbv.utils_plotting.calibration_plots(res_save_dir, calib_path, calibration, db_path)
-hbv.utils_plotting.transmission_plots(res_save_dir)
-hbv.utils_plotting.valid_plots(res_save_dir, calib_path, calibration, db_path)
-
-#hbv.utils_plotting.nnv_plot(res_save_dir)
-#hbv.utils_plotting.cost_eff_frontier(res_save_dir)
-
-owsa_runs = hbv.sensitivity.owsa_mtct_run(db_path, calib_path, calibration)
-hbv.sensitivity.owsa_mtct_outcomes(owsa_runs, res_save_dir)
